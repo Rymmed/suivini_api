c5dc12163a4914bf3bff7e68a46f762d65b211d6
RymMohamed <mhamedrim921@gmail.com>
2023-04-16 14:31:20 +0100

    auth(1)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,9 +5,52 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+
+        DB::beginTransaction();
+
+        try {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            /*$role = Role::where('name', 'user')->first();
+
+            if ($role) {
+                $user->assignRole($role);
+            }*/
+
+            DB::commit();
+
+            return response()->json(['user' => $user], 201);
+        } catch (\Exception $e) {
+            DB::rollback();
+
+            Log::error($e);
+
+            return response()->json(['error' => 'Error creating user'], 500);
+        }
+    }
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
@@ -15,7 +58,7 @@ class AuthController extends Controller
         if (Auth::attempt($credentials)) {
             // Authentication passed
             $user = Auth::user();
-            if ($user instanceof \App\Models\User) {
+            if ($user instanceof User) {
                 // Hinting here for $user will be specific to the User object
                 //return $user->createToken($request->device_name)->plainTextToken;
                 $token = $user->createToken('MyApp')->accessToken;
--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('register', [UserController::class, 'register']);
-
-//Route::post('login', [UserController::class, 'login']);
-
+Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login'])->name('login');
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
